bb9989e23d033f4ae95ce8ffca4927a187e21704
Peter VanLund <792688+petermeansrock@users.noreply.github.com>
2022-03-05 12:58:29 -0800

    feat: hide mouse cursor on launch & show with ESC

---
+++ Assets/UI/MouseCursorDisplay.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class MouseCursorDisplay : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
