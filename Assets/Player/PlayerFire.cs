7c769b000bf05d982fa35ff6070c21acda7e3be4
Peter VanLund <792688+petermeansrock@users.noreply.github.com>
2022-03-06 12:23:46 -0800

    feat: add muzzle flash to weapon

--- Assets/Player/PlayerFire.cs
+++ Assets/Player/PlayerFire.cs
@@ -2,9 +2,23 @@ using UnityEngine;
 
 public class PlayerFire : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject muzzleFlash;
+
     private void Update()
     {
+        // Toggle muzzle flash
         if (Input.GetMouseButtonDown(0))
+        {
+            muzzleFlash.SetActive(true);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            muzzleFlash.SetActive(false);
+        }
+
+        // Log raycasts
+        if (Input.GetMouseButton(0))
         {
             var ray = Camera.main.ViewportPointToRay(new(0.5f, 0.5f));
             if (Physics.Raycast(ray, out RaycastHit hitInfo, Mathf.Infinity))
